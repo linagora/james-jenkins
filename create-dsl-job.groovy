69f4a2388e63d63cd6d7bc1c756400eb4c28173a
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-07-16 16:49:28 +0200

    Issue-2 Create the workflow job a jenkins start

--- create-dsl-job.groovy
+++ create-dsl-job.groovy
@@ -1,17 +1,29 @@
+import hudson.model.FreeStyleProject;
 import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.BranchSpec;
 import hudson.triggers.SCMTrigger;
 import javaposse.jobdsl.plugin.*;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.Whitelist;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.BlanketWhitelist;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition;
 
 jenkins = Jenkins.instance;
 jenkins.getExtensionList(Whitelist.class).push(new BlanketWhitelist());
+
+scm = new GitSCM("https://github.com/linagora/james-jenkins");
+scm.branches = [new BranchSpec("*/master")];
+workflowJob = new WorkflowJob(jenkins, "workflow");
+workflowJob.definition = new CpsScmFlowDefinition(scm, "workflow-job");
+jenkins.add(workflowJob, workflowJob.name);
+
 jobName = "create-dsl-job";
 gitTrigger = new SCMTrigger("* * * * *");
-dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false", targets="build-script", scriptText=""), ignoreExisting=false, removedJobAction=RemovedJobAction.DISABLE);
+dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false", targets="build-stash-script", scriptText=""), ignoreExisting=false, removedJobAction=RemovedJobAction.DISABLE);
 dslProject = new hudson.model.FreeStyleProject(jenkins, jobName);
-dslProject.scm = new GitSCM("https://ci.open-paas.org/stash/scm/~btellier/jenkins.git");
+dslProject.scm = new GitSCM("https://github.com/linagora/james-jenkins.git");
+dslProject.scm.branches = [new BranchSpec("*/master")];
 dslProject.addTrigger(gitTrigger);
 dslProject.createTransientActions();
 dslProject.getPublishersList().add(dslBuilder);
