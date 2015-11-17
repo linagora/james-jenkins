81c2dd7143dd65aea76d58b0d5b6dffd5055a468
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-11-17 15:43:55 +0100

    Issue-18 use ldaps instead of clear-text ldap

--- use-ldap-authentication.groovy
+++ use-ldap-authentication.groovy
@@ -3,4 +3,4 @@ import hudson.security.FullControlOnceLoggedInAuthorizationStrategy;
 import hudson.security.LDAPSecurityRealm;
 
 jenkins.authorizationStrategy = new FullControlOnceLoggedInAuthorizationStrategy() ;
-jenkins.securityRealm= new LDAPSecurityRealm(server="ldap.linagora.com:389", rootDN = "", userSearchBase="", userSearch="uid={0}", groupSearchBase="", groupSearchFilter="", groupMembershipStrategy = null, managerDN = "", managerPasswordSecret = null, inhibitInferRootDN = false, disableMailAddressResolver = false, cache = null);
+jenkins.securityRealm= new LDAPSecurityRealm(server="ldaps://ldap.linagora.com", rootDN = "", userSearchBase="", userSearch="uid={0}", groupSearchBase="", groupSearchFilter="", groupMembershipStrategy = null, managerDN = "", managerPasswordSecret = null, inhibitInferRootDN = false, disableMailAddressResolver = false, cache = null);
